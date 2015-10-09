d487e039cc2ec986b5835e963a256a9dc0df51cb
Will Nielsen <wjnielsen@gmail.com>
2015-10-08 20:00:12 -0500

    Classes for lights and flags

--- app.js
+++ app.js
@@ -1,9 +1,25 @@
+var _ = require("lodash");
+var fs = require('fs');
+
 var five = require("johnny-five");
 var board = new five.Board();
 
+var flags = require('./lib/FlagAlerts.js');
+var lights = require('./lib/Lights.js');
+
 board.on("ready", function() {
-  // Create an Led on pin 13
-  var led = new five.Led(13);
-  // Blink every half second
-  led.blink(500);
+
+  flags.make('green', 13);
+  flags.make('red', 12);
+  lights.make('green', 8);
+  lights.make('red', 7);
+
+
+
+  // Add to REPL (optional)
+  this.repl.inject({
+    flags: flags,
+    lights: lights
+  });
+
 });
